cba39cf9e7bdeff7b25060fa3e3c106b865d432d
vikions <plambir1994@mail.ru>
2026-02-15 11:58:46 +0100

    landing2

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias ?? {}),
+      "@react-native-async-storage/async-storage": false,
+      "pino-pretty": false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
